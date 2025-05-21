909e4d67987eacf2b270f34b0e9ca7271afd6008
alejandrossm <alejandrossm@outlook.com>
2025-05-21 14:55:43 -0400

    Tabla Reserva updated

--- reserva/src/main/java/com/comunicacion/reserva/model/Reserva.java
+++ reserva/src/main/java/com/comunicacion/reserva/model/Reserva.java
@@ -12,7 +12,13 @@ public class Reserva {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idReserva;
+
+    @Column(nullable = false)
     private int mesa;
+
+    @Column(nullable = false)
     private Long idCliente;
+    
+    @Column(nullable = false, length = 250)
     private String correoCliente;
 }
